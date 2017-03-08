02750879470bc25c9bd3592e921338d0ce0bbd64
sharon smith <sharon.e.smith2@gmail.com>
2017-03-07 21:41:28 -0600

    initial succesful writing to DOM

--- food.js
+++ food.js
@@ -1,2 +1,45 @@
 
 console.log("writing to console");
+
+var dogFoodContainer = document.getElementById("dogFoodProducts");
+function makeDOM(xhrData){
+
+	var productString = "";
+	var currentpProduct;
+
+	for (var i=0; i<xhrData.dog_brands.length; i++) {
+		currentProduct = xhrData.dog_brands[i];
+		
+			productString += `<div class="foodName">`;
+			productString += `<h3 class="brandName">${currentProduct.name}</h3>`;
+// 			productString += `<p>Is a ${currentProduct.type}</p>`;
+// 			productString += `<p>Like to eat: ${currentProduct.food}</p>`;     
+			productString += `</div>`;
+	} // for loop
+	dogFoodContainer.innerHTML = productString;
+}
+
+function executeThisCodeAfterFileLoaded(){
+
+	var data = JSON.parse(this.responseText);
+	console.log("parsed data :: ", data);
+	makeDOM(data);
+}
+
+function executeThisCodeAfterFileFails(){
+	console.log("whoops! error ... ");
+}
+
+
+
+var myRequest = new XMLHttpRequest();
+
+myRequest.addEventListener("load", executeThisCodeAfterFileLoaded);
+myRequest.addEventListener("error", executeThisCodeAfterFileFails);
+myRequest.open("GET", "dogs.json");
+myRequest.send();
+console.log("myRequest", myRequest);
+
+
+
+
