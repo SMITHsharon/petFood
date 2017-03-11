7bf5693baa44e6266c253ed35b2fa4bc14870c09
sharon smith <sharon.e.smith2@gmail.com>
2017-03-11 13:21:48 -0600

    dog food functionality is complete

--- food.js
+++ food.js
@@ -4,7 +4,7 @@ console.log("writing to console");
 var dogFoodContainer = document.getElementById("dogFoodProducts");
 function makeDOM(xhrData){
 
-	var productString = "";
+	var productString = `<h1>Dog<br>Food<br>Products</h1>`;
 	var currentpProduct;
 
 	for (var i=0; i<xhrData.dog_brands.length; i++) {
@@ -12,11 +12,39 @@ function makeDOM(xhrData){
 		
 			productString += `<div class="foodName">`;
 			productString += `<h3 class="brandName">${currentProduct.name}</h3>`;
-// 			productString += `<p>Is a ${currentProduct.type}</p>`;
-// 			productString += `<p>Like to eat: ${currentProduct.food}</p>`;     
+
+			for (var j=0; j<currentProduct.types.length; j++) {
+				productString += `<p class="thisType">${cleanedProductString(currentProduct.types[j].type)}</p>`;
+				// productString += `<p class="thisType">${currentProduct.types[j].type.replace("_", " ").charAt(0).toUpperCase()}</p>`;
+
+				for (var k=0; k<currentProduct.types[j].volumes.length; k++) {
+					productString += `<p class="thisPrice">${currentProduct.types[j].volumes[k].name}: `
+					productString += `$${currentProduct.types[j].volumes[k].price}</p>`;
+				} // k loop
+			} // j loop	
+
 			productString += `</div>`;
-	} // for loop
+	} // i loop
+
 	dogFoodContainer.innerHTML = productString;
+}
+
+function cleanedProductString (foodString) {
+	
+console.log("foodString :: ", foodString);
+	tempStr = foodString.replace("_", " ");
+	secondCap = false;
+	if (tempStr.includes(" ") === true) {
+		secondCap = true;
+		capIndex = tempStr.indexOf(" ")+1;
+	}
+
+	tempStr = foodString.charAt(0).toUpperCase() + tempStr.substr(1);
+	if (secondCap) {
+		tempStr = tempStr.substr(0, capIndex) + foodString.charAt(capIndex).toUpperCase() + tempStr.substr(5);
+	}
+	
+	return tempStr;
 }
 
 function executeThisCodeAfterFileLoaded(){
